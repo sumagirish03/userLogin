790970c448a5de334da20b97c092e12181ce3507
suma <sumahm@gmail.com>
2023-01-18 22:48:16 +0530

    missing controller changes added

--- app/controllers/userlogin_controller.rb
+++ app/controllers/userlogin_controller.rb
@@ -4,27 +4,49 @@ class UserloginController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        current_user = User.find_by_id(session)[:current_user_id]
+       # current_user = Userlogin.find_by_id(session)[:current_user_id]
         #render html: "hello db"
         render json: Userlogin.all 
+        #render json: current_user
     end
-    def show 
+    def show
         v = Userlogin.find(params[:id])
         render json: v
     end
     def create
-          #v=!params[:name].empty? and !params[:email].empty? and params[:address].empty? and !params[:password].empty? and !params[:phone_number].empty? and !params[:role_id].nil?
+          #v=!params[:username].empty? and !params[:useremail].empty? and !params[:password].empty?
           v = !params[:username].empty?
           if (v)
             p = Userlogin.create(
               'username': params[:username],
               'useremail': params[:useremail],
-              'password': params[:password]
+              'password': params[:useremail]
             )
             puts p
             render json: "User Created"
           else
           render json: "User does not exist"
           end
+    end
+    def update
+        v = Userlogin.exists?(params[:id])
+        puts v
+        if (v)
+          s = Userlogin.find_by_id(params[:id])
+          s.update('username': params[:username], 'useremail': params[:useremail], 'password': params[:password])
+          render json: "User details updated"
+        else
+          render json: "User does not exist"
+        end
+    end
+    def destroy
+      v = Userlogin.exists?(params[:id])
+      if(v)
+        u = Userlogin.find_by_id(params[:id])
+        u.destroy
+        render json: "User deleted"
+      else
+        render json: "User not found"
       end
     end
+end
